b49a778811450bddd84307b628022595e68c99fd
Gabrielhlc <gabrielhenlc@gmail.com>
2023-03-17 17:46:47 -0300

    feat: authentication

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,17 @@
+import { AuthContext } from "@/contexts/AuthContext"
+import { api } from "@/services/api";
+import { useContext, useEffect } from "react"
+
+export default function Dashboard() {
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        api.get('/me')
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+    }, [])
+
+    return (
+        <h1>Dashboard: {user.email}</h1>
+    )
+}
